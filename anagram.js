d8ffc8c777ebe5ef41de839a1a2391e149e8926b
Amanda Treutler <amandathedev@gmail.com>
2019-10-16 09:05:45 -0500

    anagram fix

--- anagram.js
+++ anagram.js
@@ -3,14 +3,14 @@ let isAnagram = (word1, word2) => {
 
   if (word1.length !== word2.length) {
     return false;
-  }
+  } else {
+    for (let char of word1) {
+      counter[char] ? (counter[char] += 1) : (counter[char] = 1);
+    }
 
-  for (let char of word1) {
-    counter[char] ? (counter[char] += 1) : (counter[char] = 1);
-  }
-
-  for (let char of word2) {
-    counter[char] ? (counter[char] -= 1) : (counter[char] = 1);
+    for (let char of word2) {
+      counter[char] ? (counter[char] -= 1) : false;
+    }
   }
 
   for (let key in counter) {
