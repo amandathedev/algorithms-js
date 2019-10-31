5548d9777b0f9b6ca9b4945bfbbcc9a8cb560d8e
Amanda Treutler <amandathedev@gmail.com>
2019-10-31 11:15:18 -0500

    Who is next

---
+++ whoIsNext.js
@@ -0,0 +1,11 @@
+let names = ["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"];
+function whoIsNext(names, r) {
+  for (let i = 1; i < r; i++) {
+    let x = names.shift();
+    names.push(x, x);
+  }
+
+  return names[0];
+}
+
+// whoIsNext(names, 52)
