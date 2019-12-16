137f4b2c5807209f5f3b4be6ce443a7d5a3d891b
Amanda Treutler <amandathedev@gmail.com>
2019-12-16 11:01:21 -0600

    redo

---
+++ anagram2.js
@@ -0,0 +1,27 @@
+const anagramCheck = (word1, word2) => {
+  let counter = {};
+
+  if (word1.length !== word2.length) {
+    return false;
+  } else {
+    for (let char of word1) {
+      counter[char] ? (counter[char] += 1) : (counter[char] = 1);
+    }
+
+    for (let char of word2) {
+      counter[char] ? (counter[char] -= 1) : false;
+    }
+
+    for (let key in counter) {
+      if (counter[key] !== 0) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  }
+};
+
+anagramCheck("whale", "while");
+// anagramCheck('stressed', 'dessert')
+// anagramCheck('rats', 'star')
---
+++ reverseString.js
@@ -0,0 +1,5 @@
+const reverseString = str => {
+  let newString = "";
+};
+
+reverseString("this is facing the wrong way");
