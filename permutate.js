6199f74b4f950fbb3901b80ec4d81fad7d6a44ca
Amanda Treutler <amandathedev@gmail.com>
2019-12-19 20:58:31 -0600

    permutate

---
+++ permutate.js
@@ -0,0 +1,18 @@
+var permArr = [],
+  usedChars = [];
+function permute(input) {
+  var i, ch;
+  for (i = 0; i < input.length; i++) {
+    ch = input.splice(i, 1)[0];
+    usedChars.push(ch);
+    if (input.length == 0) {
+      permArr.push(usedChars.slice());
+    }
+    permute(input);
+    input.splice(i, 0, ch);
+    usedChars.pop();
+  }
+  return permArr;
+}
+
+permute(["H", "E", "L", "L", "O"]);
