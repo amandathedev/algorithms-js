bd3b0798b7e4dd3043cd2dc488aa6006db493c9b
Amanda Treutler <amandathedev@gmail.com>
2020-01-02 16:22:46 -0600

    average pair

--- averagePair3.js
+++ averagePair3.js
@@ -1,3 +1,10 @@
-const averagePair = (arr, num) => {};
+const averagePair = (arr, num) => {
+  let start = 0;
+  let end = arr.length - 1;
 
-averagePair([1, 2, 3, 4, 5], 1.5);
+  while (start < end) {
+    let average = (arr[start] + arr[end]) / 2;
+  }
+};
+
+averagePair([-2, 0, 1, 2, 4, 5, 6, 7], 5.5);
