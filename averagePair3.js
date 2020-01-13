b7b11406cff7662b1cdb23d266aaa5cada0c964b
Amanda Treutler <amandathedev@gmail.com>
2020-01-13 12:00:53 -0600

    max subarray

--- averagePair3.js
+++ averagePair3.js
@@ -4,7 +4,15 @@ const averagePair = (arr, num) => {
 
   while (start < end) {
     let average = (arr[start] + arr[end]) / 2;
+    if (average === num) {
+      return `${arr[start]} and ${arr[end]} have an average of ${num}`;
+    } else if (average > num) {
+      end--;
+    } else {
+      start++;
+    }
   }
+  return `None of these number pairs have an average of ${num}`;
 };
 
 averagePair([-2, 0, 1, 2, 4, 5, 6, 7], 5.5);
---
+++ maxSubarraySum2.js
@@ -0,0 +1,22 @@
+const maxSubarraySum = (arr, num) => {
+  let tempSum = 0;
+  let maxSum = 0;
+
+  if (arr.length < num) {
+    return null;
+  }
+
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
+  }
+
+  tempSum = maxSum;
+
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  return maxSum;
+};
+
+maxSubarraySum([1, 3, 5, 6, 3, 5, 7, 10, 2, 8, 9], 3);
