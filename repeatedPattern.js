f7f3533764d9abe280b90b707f49a83057819458
Amanda Treutler <amandathedev@gmail.com>
2020-01-17 08:37:59 -0600

    repeated pattern

--- repeatedPattern.js
+++ repeatedPattern.js
@@ -27,3 +27,36 @@ function repeatedSubstringPattern(str) {
 
 // repeatedSubstringPattern('ababab')
 // not working
+
+function repeatedPattern(str) {
+  let substring = "";
+  let i = 0;
+  let l = 0;
+
+  while (i < str.length / 2) {
+    substring += str[i];
+    l++;
+    let j = i + 1;
+    while (j < str.length) {
+      let temp = str.slice(j, j + l);
+      if (temp !== substring) {
+        break;
+      }
+      j += l;
+    }
+    i++;
+    if (j === str.length) return true;
+  }
+  return false;
+}
+
+// repeatedPattern('a')
+
+function repeat(s) {
+  return s
+    .repeat(2)
+    .slice(1, -1)
+    .includes(s);
+}
+
+// repeat('abcabc')
