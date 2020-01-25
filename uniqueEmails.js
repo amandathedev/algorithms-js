1b2828c600811fa77522394e8ccbaaad653d9d11
Amanda Treutler <amandathedev@gmail.com>
2020-01-25 16:22:46 -0600

    unique emails

---
+++ uniqueEmails.js
@@ -0,0 +1,32 @@
+const uniqueEmails = emails => {
+  let reformatted = new Set();
+
+  for (let i = 0; i < emails.length; i++) {
+    let [localName, domain] = emails[i].split("@");
+    let splitName = localName.split("");
+    domain = "@".concat(domain);
+    let newName = "";
+
+    for (let j = 0; j < splitName.length; j++) {
+      if (splitName[j] !== ".") {
+        if (splitName[j] === "+") {
+          break;
+        } else {
+          newName += splitName[j];
+        }
+      }
+    }
+    newName = newName.concat(domain);
+    reformatted.add(newName);
+  }
+  return reformatted.size;
+};
+
+uniqueEmails([
+  "amanda.t@gmail.com",
+  "amandat@gmail.com",
+  "hello+bye@yahoo.com",
+  "hello@yahoo.com",
+  "hello@gmail.com",
+  "he.llo@gmail.com"
+]);
