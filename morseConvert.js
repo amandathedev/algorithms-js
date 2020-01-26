93c7eb79b7403ba0c6145e6956009c17dfc57370
Amanda Treutler <amandathedev@gmail.com>
2020-01-26 12:31:34 -0600

    morse converter

---
+++ morseConvert.js
@@ -0,0 +1,78 @@
+const morseConvert = text => {
+  let morseAlphabet = [
+    ".-",
+    "-...",
+    "-.-.",
+    "-..",
+    ".",
+    "..-.",
+    "--.",
+    "....",
+    "..",
+    ".---",
+    "-.-",
+    ".-..",
+    "--",
+    "-.",
+    "---",
+    ".--.",
+    "--.-",
+    ".-.",
+    "...",
+    "-",
+    "..-",
+    "...-",
+    ".--",
+    "-..-",
+    "-.--",
+    "--.."
+  ];
+
+  let alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+  ];
+
+  let alphaObj = Object.assign(
+    ...alphabet.map((k, i) => ({ [k]: morseAlphabet[i] }))
+  );
+
+  let letters = [...text.toLowerCase()];
+  let answer = "";
+
+  for (let char of letters) {
+    if (!alphaObj[char]) {
+      answer += "";
+    } else {
+      answer += alphaObj[char] + " ";
+    }
+  }
+
+  return answer;
+};
+
+morseConvert("Hello my name is Amanda");
